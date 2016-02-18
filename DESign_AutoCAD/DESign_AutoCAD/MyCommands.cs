044ff26b72f6b3cecafedf244a2c1b07042d8e7c
dariens <darien.shannon@gmail.com>
2016-02-17 16:44:40 -0800

    Added "TCWIDTHS_JOISTS_EXCLUDE_5_IN" command
    For use when the typical TC width for joists is 5 in.

--- DESign_AutoCAD/DESign_AutoCAD/MyCommands.cs
+++ DESign_AutoCAD/DESign_AutoCAD/MyCommands.cs
@@ -178,6 +178,86 @@ namespace DESign_AutoCAD
             Database db = doc.Database;
         }
 
+        [CommandMethod("MyCommandGroup", "TCWIDTHS_JOISTS_EXCLUDE_5_IN", CommandFlags.Modal)]
+        public void tcWidths_joists_no_5_in()
+        {
+            Job job1 = new Job();
+            job1 = joistDetails.JobFromShoporderJoistDetails();
+            List<Joist> joists = job1.Joists;
+
+            foreach (Joist joist in joists)
+            {
+                if (joist.TCWidth != "5")
+                {
+                    string markWithWidth = joist.Mark + " (" + joist.TCWidth + ")";
+                    Document doc = Application.DocumentManager.MdiActiveDocument;
+                    Database db = doc.Database;
+                    using (Transaction tr = db.TransactionManager.StartTransaction())
+                    {
+                        BlockTableRecord btr = (BlockTableRecord)tr.GetObject
+                            (SymbolUtilityServices.GetBlockModelSpaceId(db), OpenMode.ForRead);
+
+                        foreach (ObjectId id in btr)
+                        {
+                            Entity currentEntity = tr.GetObject(id, OpenMode.ForWrite, false) as Entity;
+                            if (currentEntity == null)
+                            {
+                                continue;
+                            }
+                            if (currentEntity.GetType() == typeof(MText))
+                            {
+                                string strippedText = Regex.Replace(((MText)currentEntity).Contents, " ", "");
+                                if (strippedText == joist.Mark)
+                                {
+                                    MText mText = (MText)currentEntity;
+                                    string mTextContent = mText.Contents;
+                                    string newmTextContent = Regex.Replace(mTextContent, joist.Mark, markWithWidth);
+                                    ((MText)currentEntity).Contents = newmTextContent;
+                                }
+                            }
+                            if (currentEntity.GetType() == typeof(DBText))
+                            {
+                                string strippedText = Regex.Replace(((DBText)currentEntity).TextString, " ", "");
+                                if (strippedText == joist.Mark)
+                                {
+                                    DBText dbText = (DBText)currentEntity;
+                                    string dbTextString = dbText.TextString;
+                                    string newdbTextString = Regex.Replace(dbTextString, joist.Mark, markWithWidth);
+                                    ((DBText)currentEntity).TextString = newdbTextString;
+                                }
+                            }
+                            if (currentEntity.GetType() == typeof(RotatedDimension))
+                            {
+                                string dimText = ((RotatedDimension)currentEntity).DimensionText;
+
+                                if (dimText.Contains(string.Format("-{0}\\X", joist.Mark)) == true)
+                                {
+                                    string replace = string.Format("-{0}", joist.Mark);
+                                    string replacement = string.Format("-{0}", markWithWidth);
+                                    string newrdText = Regex.Replace(dimText, replace, replacement);
+                                    ((RotatedDimension)currentEntity).DimensionText = newrdText;
+                                }
+
+                                string[] dimTextArray = Regex.Split(dimText, "-");
+                                string mark = dimTextArray[dimTextArray.Length - 1];
+
+                                if (mark == joist.Mark)
+                                {
+                                    string replace = string.Format("-{0}", joist.Mark);
+                                    string replacement = string.Format("-{0}", markWithWidth);
+                                    string newrdText = Regex.Replace(dimText, replace, replacement);
+                                    ((RotatedDimension)currentEntity).DimensionText = newrdText;
+                                }
+
+                            }
+
+                        }
+                        tr.Commit();
+                    }
+                }
+            }
+        }
+
         [CommandMethod("MyCommandGroup", "TCWIDTHS_ALL", CommandFlags.Modal)]
         public void tcWidths_ALL()
         {
