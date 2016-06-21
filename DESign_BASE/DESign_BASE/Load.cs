e5437818b750383f58bbd3b84c31e0a60e3c78c0
dariens <darien.shannon@gmail.com>
2016-06-21 10:15:55 -0700

    Added properties to Load class

--- DESign_BASE/DESign_BASE/Load.cs
+++ DESign_BASE/DESign_BASE/Load.cs
@@ -6,9 +6,51 @@ namespace DESign_BASE
 {
     public class Load
     {
+        enum Types {U, NU, GU, C, CB, CP, CA, C3, CZ, AX, M, S};
+        public string Type
+        {
+            get { return Type; }
+            set
+            {
+                if(Enum.GetNames(typeof(Types)).Contains(value) == true)
+                {
+                    Type = value;
+                }
+            }
+        }
+        enum Categories { TL, DL, LL, WL, SL, CL, SM, IP }
+        public string Category
+        {
+            get { return Category; }
+            set
+            {
+                if (Enum.GetNames(typeof(Categories)).Contains(value) == true)
+                {
+                    Category = value;
+                }
+            }
+        }
+
+        enum Positions { TC, BC, BE, LE, RE }
+        public string Position
+        {
+            get { return Position; }
+            set
+            {
+                if (Enum.GetNames(typeof(Positions)).Contains(value) == true)
+                {
+                    Position = value;
+                }
+            }
+        }
+
+        public string Reference { get; set; }
+        public int Group { get; set; }
         public double StartValue { get; set; }
         public double StartLocation { get; set; }
         public double EndValue { get; set; }
         public double EndLocation { get; set; }
     }
+
+    
 }
