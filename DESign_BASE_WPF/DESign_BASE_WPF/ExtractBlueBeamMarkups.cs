b930b3465094b547bdc59c22c714bbf4c6aa59e4
dariens <darien.shannon@gmail.com>
2016-08-17 08:24:34 -0700

    Added Multiselect in ExtractBlueBeamMarkups

--- DESign_BASE_WPF/DESign_BASE_WPF/ExtractBlueBeamMarkups.cs
+++ DESign_BASE_WPF/DESign_BASE_WPF/ExtractBlueBeamMarkups.cs
@@ -22,73 +22,79 @@ namespace DESign_BASE_WPF
 
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Title = "SELECT BLUE BEAM MARKUP FILE (.XML)";
-
             openFileDialog.Filter = "Markup File (.xml)|*.xml";
+            openFileDialog.Multiselect = true;
+
             if (openFileDialog.ShowDialog() == true)
             {
-                string fileName = openFileDialog.FileName;
-
-                //CREATE A COPY OF THE SELECTED FILE AND STORE IT IN A TEMPORARY FILE
-                string markupFileName = System.IO.Path.GetTempFileName();
-                Byte[] markupInByteArray = System.IO.File.ReadAllBytes(openFileDialog.FileName);
-                System.IO.File.WriteAllBytes(markupFileName, markupInByteArray);
-
-                //LOAD THE BLUEBEAM MARKUP FILE INTO AN XElement
-                XElement markUpFile = XElement.Load(markupFileName);
-
-                //QUERY THE MARKUP XElement TO FIND ALL GIRDER MARKUPS
-                var girderMarkups =
-                    from el in markUpFile.Elements("Markup")
-                    where (string)el.Element("Subject") == "GIRDER" 
-                    select el;
-
-                //QUERY THE MARKUP XElement TO FIN ALL JOIST MARKUPS
-                var joistMarkups =
-                    from el in markUpFile.Elements("Markup")
-                    where (string)el.Element("Subject") == "JOIST"
-                    select el;
-
-
-                //EXTRACT ALL GIRDERS AND THEIR VALUES FROM THE QUERRIED GIRDERS
-                List<Girder> girders = new List<Girder>();  
-                foreach (XElement girderMarkup in girderMarkups)
-                {
-                    Girder girder = new Girder();
-
-                    girder.Mark = (string)girderMarkup.Element("Label");
-                    girder.Description = (string)girderMarkup.Element("JOIST_DESC");
-                    girder.Quantity = (int)girderMarkup.Element("Count");
-
-                    string allNotes = (string)girderMarkup.Element("NOTES");
-                    girder.Notes = Regex.Split(allNotes, "\n").ToList();
-
-                    string allLoads = (string)girderMarkup.Element("LOADS");
-                    girder.Loads = Regex.Split(allLoads, "\n").ToList();
-
-
-                    girder.strBaseLength = (string)girderMarkup.Element("J-G_Length");
-                    girders.Add(girder);
-                }
-
-                //EXTRACT ALL JOISTS AND THEIR VALUES FROM THE QUERRIED JOISTS
+                List<Girder> girders = new List<Girder>();
                 List<Joist> joists = new List<Joist>();
-                foreach (XElement joistMarkup in joistMarkups)
+
+                foreach (String fileName in openFileDialog.FileNames)
                 {
-                    Joist joist = new Joist();
 
-                    joist.Mark = (string)joistMarkup.Element("Label");
-                    joist.Description = (string)joistMarkup.Element("JOIST_DESC");
-                    joist.Quantity = (int)joistMarkup.Element("Count");
+                    //CREATE A COPY OF THE SELECTED FILE AND STORE IT IN A TEMPORARY FILE
+                    string markupFileName = System.IO.Path.GetTempFileName();
+                    Byte[] markupInByteArray = System.IO.File.ReadAllBytes(fileName);
+                    System.IO.File.WriteAllBytes(markupFileName, markupInByteArray);
 
-                    string allNotes = (string)joistMarkup.Element("NOTES");
-                    joist.Notes = Regex.Split(allNotes, "\n").ToList();
+                    //LOAD THE BLUEBEAM MARKUP FILE INTO AN XElement
+                    XElement markUpFile = XElement.Load(markupFileName);
 
-                    string allLoads = (string)joistMarkup.Element("LOADS");
-                    joist.Loads = Regex.Split(allLoads, "\n").ToList();
+                    //QUERY THE MARKUP XElement TO FIND ALL GIRDER MARKUPS
+                    var girderMarkups =
+                        from el in markUpFile.Elements("Markup")
+                        where (string)el.Element("Subject") == "GIRDER"
+                        select el;
 
-                    joist.strBaseLength = (string)joistMarkup.Element("J-G_Length");
+                    //QUERY THE MARKUP XElement TO FIN ALL JOIST MARKUPS
+                    var joistMarkups =
+                        from el in markUpFile.Elements("Markup")
+                        where (string)el.Element("Subject") == "JOIST"
+                        select el;
 
-                    joists.Add(joist);
+
+                    //EXTRACT ALL GIRDERS AND THEIR VALUES FROM THE QUERRIED GIRDERS
+
+                    foreach (XElement girderMarkup in girderMarkups)
+                    {
+                        Girder girder = new Girder();
+
+                        girder.Mark = (string)girderMarkup.Element("Label");
+                        girder.Description = (string)girderMarkup.Element("JOIST_DESC");
+                        girder.Quantity = (int)girderMarkup.Element("Count");
+
+                        string allNotes = (string)girderMarkup.Element("NOTES");
+                        girder.Notes = Regex.Split(allNotes, "\n").ToList();
+
+                        string allLoads = (string)girderMarkup.Element("LOADS");
+                        girder.Loads = Regex.Split(allLoads, "\n").ToList();
+
+
+                        girder.strBaseLength = (string)girderMarkup.Element("J-G_Length");
+                        girders.Add(girder);
+                    }
+
+                    //EXTRACT ALL JOISTS AND THEIR VALUES FROM THE QUERRIED JOISTS
+
+                    foreach (XElement joistMarkup in joistMarkups)
+                    {
+                        Joist joist = new Joist();
+
+                        joist.Mark = (string)joistMarkup.Element("Label");
+                        joist.Description = (string)joistMarkup.Element("JOIST_DESC");
+                        joist.Quantity = (int)joistMarkup.Element("Count");
+
+                        string allNotes = (string)joistMarkup.Element("NOTES");
+                        joist.Notes = Regex.Split(allNotes, "\n").ToList();
+
+                        string allLoads = (string)joistMarkup.Element("LOADS");
+                        joist.Loads = Regex.Split(allLoads, "\n").ToList();
+
+                        joist.strBaseLength = (string)joistMarkup.Element("J-G_Length");
+
+                        joists.Add(joist);
+                    }
                 }
 
                 //CREATE EXCEL COM OBJECT
@@ -109,6 +115,7 @@ namespace DESign_BASE_WPF
                 girders = girders.OrderBy(x => x.StrippedNumber).ToList();
                 job.Joists = joists;
                 job.Girders = girders;
+
             }
             return job;
         }
